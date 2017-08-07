72463b7d029d111b69e698bbc3988dfe0527037b
DianeW0223 <dwatts@motor.com>
2017-08-07 15:37:27 -0400

    add sql database

---
+++ Controllers/ArtInfoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Final_Project.Models;
+
+namespace Final_Project.Controllers
+{
+    public class ArtInfoesController : Controller
+    {
+        private ArtInfoEntities db = new ArtInfoEntities();
+
+        // GET: ArtInfoes
+        public ActionResult Index()
+        {
+            return View(db.ArtInfoes.ToList());
+        }
+
+        // GET: ArtInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ArtInfo artInfo = db.ArtInfoes.Find(id);
+            if (artInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artInfo);
+        }
+
+        // GET: ArtInfoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ArtInfoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Artist,Year,Genre,Medium,ImageFile")] ArtInfo artInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ArtInfoes.Add(artInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(artInfo);
+        }
+
+        // GET: ArtInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ArtInfo artInfo = db.ArtInfoes.Find(id);
+            if (artInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artInfo);
+        }
+
+        // POST: ArtInfoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Artist,Year,Genre,Medium,ImageFile")] ArtInfo artInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(artInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(artInfo);
+        }
+
+        // GET: ArtInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ArtInfo artInfo = db.ArtInfoes.Find(id);
+            if (artInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artInfo);
+        }
+
+        // POST: ArtInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ArtInfo artInfo = db.ArtInfoes.Find(id);
+            db.ArtInfoes.Remove(artInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace Final_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        string APIkey = "?apikey=db4038a0-79da-11e7-aa25-e32c9c02c857";
+
+        public ActionResult Data()
+        {
+            string size = "?height=300&width=300";
+            string param = "/exhibition/4236";
+            HttpWebRequest request = WebRequest.CreateHttp("http://api.harvardartmuseums.org" + param + APIkey);
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string ApiText = rd.ReadToEnd();
+            JObject o = JObject.Parse(ApiText);
+            ViewBag.ApiText = o["images"][0]["baseimageurl"] + size;
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
